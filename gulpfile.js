19b4a3574ae3482bf6fc7e8a106275c220b4c02e
Dmytro <dimasukgaborak@gmail.com>
2023-10-17 18:54:59 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,29 @@ const gulp = require('gulp'),
 
 /* Мініфікація CSS файлів */
 gulp.task('minCSS', async function(){
-  gulp.src('app/css/*.scss') // виберіть файли, які потрібно перемістити
+  gulp.src('./app/css/*.scss') // виберіть файли, які потрібно перемістити
       .pipe(sass())
       .pipe(minifyCSS())
       .pipe(rename({
         suffix: '.min'
       }))
-      .pipe(gulp.dest('public/css')) // вказати цільову директорію переміщення
+      .pipe(gulp.dest('./public/css')) // вказати цільову директорію переміщення
       .pipe(browserSync.stream()); // ф-ц передає дані в браузер сінк і оновлює сторінку
 });
 
 /* Мініфікація JS файлів */
 gulp.task('minJS', async function(){
-  gulp.src('app/js/main.js') // виберіть файли, які потрібно перемістити
+  gulp.src('./app/js/main.js') // виберіть файли, які потрібно перемістити
       .pipe(minifyJS())
-      .pipe(gulp.dest('public/js')) // вказати цільову директорію переміщення
+      .pipe(gulp.dest('./public/js')) // вказати цільову директорію переміщення
       .pipe(browserSync.stream());
 });
 
 /*  Відслідкування змін в файлах */
 gulp.task('watchAll', function () {
-    gulp.watch("app/css/*.scss", gulp.series('minCSS'));
+    gulp.watch("./app/css/*.scss", gulp.series('minCSS'));
     // series - допомагає виконувати задачі почерзі
-    gulp.watch("app/js/*.js", gulp.series('minJS'));
+    gulp.watch("./app/js/*.js", gulp.series('minJS'));
 });
 
 /* Для автоматичної синхронізації оновлення сторінки */
