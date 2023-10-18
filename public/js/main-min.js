1a80964aca05a61829ff5fae47e747e272ec139c
dimaa21 <dimasukgaborak@gmail.com>
2023-10-18 14:51:56 +0300

    Create main-min.js

---
+++ public/js/main-min.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelector(".main").classList.add("show"),document.querySelector(".first-block").classList.add("show"),document.querySelector(".second-block").classList.add("show"),document.querySelector(".third-block").classList.add("show")});
