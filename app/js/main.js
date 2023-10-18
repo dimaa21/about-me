4458cc9df31bc9b0d626223e470aa7e60fe02049
dimaa21 <dimasukgaborak@gmail.com>
2023-10-18 14:52:10 +0300

    Update main.js

---
+++ app/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Додайте клас `show` до кожного елемента, який ви хочете анімувати
+  document.querySelector('.main').classList.add('show');
+  document.querySelector('.first-block').classList.add('show');
+  document.querySelector('.second-block').classList.add('show');
+  document.querySelector('.third-block').classList.add('show');
+});
