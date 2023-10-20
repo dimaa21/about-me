b319e9260a61c0645b577019166210f940e6835f
dimaa21 <dimasukgaborak@gmail.com>
2023-10-20 17:20:59 +0300

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.social').classList.add('animated');
   document.querySelector('.about-me').classList.add('animated');
 });
+
+function slowScroll(id) {
+    $("html, body").animate({
+        scrollTop: $(id).offset().top - 50
+    }, 500);
+    return false;
+}
+
+$(".header-top .menu").on("click", function() {
+    if($("header .mobile-menu").is(":visible"))
+        $(this).html('<i class="fas fa-bars"></i>');
+    else
+        $(this).html('<i class="fas fa-times"></i>');
+
+    $("header .mobile-menu").slideToggle();
+})
