ad1bb05d74e258b2a91a276f23a8542822fef2f2
Dmytro <dimasukgaborak@gmail.com>
2023-10-21 18:57:17 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -21,3 +21,38 @@ $(".header-top .menu").on("click", function() {
 
     $("header .mobile-menu").slideToggle();
 })
+
+  const reviews = document.querySelectorAll('.review');
+  let currentReview = 0;
+
+  function showReview(index) {
+    if (index < 0) {
+      currentReview = reviews.length - 1;
+    } else if (index >= reviews.length) {
+      currentReview = 0;
+    }
+
+    reviews.forEach((review, i) => {
+      if (i === currentReview) {
+        review.style.display = 'block';
+      } else {
+        review.style.display = 'none';
+      }
+    });
+  }
+
+  function nextReview() {
+    showReview(currentReview + 2);
+  }
+
+  function prevReview() {
+    showReview(currentReview - 1);
+  }
+
+  // Відображення першого відгуку
+  showReview(currentReview);
+
+  // Додавання обробників подій для кнопок навігації (опціонально)
+  document.querySelector('.prev').addEventListener('click', prevReview);
+  document.querySelector('.next').addEventListener('click', nextReview);
+
