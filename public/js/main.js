08d8d105339b86a807d21325a2ae284abcffe642
dimaa21 <dimasukgaborak@gmail.com>
2023-10-23 17:20:42 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -13,41 +13,6 @@ function slowScroll(id) {
     return false;
 }
 
-$(".header-top .menu").on("click", function() {
-    if($("header .mobile-menu").is(":visible"))
-        $(this).html('<i class="fas fa-bars"></i>');
-    else
-        $(this).html('<i class="fas fa-times"></i>');
 
-    $("header .mobile-menu").slideToggle();
-})
-
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slide');
-let index = 0;
-
-function updateSlider() {
-    slider.style.transform = `translateX(-${index * 100}%)`;
-}
-
-function nextSlide() {
-    if (index < slides.length - 1) {
-        index++;
-        updateSlider();
-    }
-}
-
-function prevSlide() {
-    if (index > 0) {
-        index--;
-        updateSlider();
-    }
-}
-
-document.querySelector('.prev-button').addEventListener('click', prevSlide);
-document.querySelector('.next-button').addEventListener('click', nextSlide);
-
-// Automatic slide change
-setInterval(nextSlide, 5000);
 
 
