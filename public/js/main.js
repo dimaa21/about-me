df5efbaa8a979a908e129db1fec25aadd7c2adbe
Dmytro Haborak <dimasukgaborak@gmail.com>
2023-11-24 13:37:24 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -30,14 +30,11 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal(`.home__data, .home__img,
-            .about__data, .about__img,
-            .services__content, .menu__content,
-            .app__data, .app__img,
-            .contact__data, .contact__button,
-            .footer__content`, {
+sr.reveal(`.header-main, .about-me,
+            .services, .form`, {
     interval: 200
 })
+
 
 
 
